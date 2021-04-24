273bbb1bcab238da10304aa82354a45da7743b81
Luiz Guilherme P. Santos <luizgpsantos@gmail.com>
2021-04-24 09:37:01 -0300

    quiet

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -17,7 +17,7 @@ race () {
     clear_cache_wait
 }
 
-INSTALLATION_ID=`esrally install --distribution-version=7.12.0 --node-name="rally-node-0" --network-host="127.0.0.1" --http-port=39200 --master-nodes="rally-node-0" --seed-hosts="127.0.0.1:39300" --car="4gheap,x-pack-monitoring-http" --car-params=car_params.json | jq --raw-output '.["installation-id"]'`
+INSTALLATION_ID=`esrally install --quiet --distribution-version=7.12.0 --node-name="rally-node-0" --network-host="127.0.0.1" --http-port=39200 --master-nodes="rally-node-0" --seed-hosts="127.0.0.1:39300" --car="4gheap,x-pack-monitoring-http" --car-params=car_params.json | jq --raw-output '.["installation-id"]'`
 
 export RACE_ID=$(uuidgen)
 esrally start --installation-id="${INSTALLATION_ID}" --race-id="${RACE_ID}"
