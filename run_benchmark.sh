5071d0baecfaa93632729491c2d6ef5a56f81448
Luiz Guilherme P. Santos <luizgpsantos@gmail.com>
2021-04-24 18:51:44 -0300

    create race ID for each challenge

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -13,13 +13,12 @@ clear_cache_wait () {
 }
 
 race () {
-    esrally race --pipeline=benchmark-only --target-host=127.0.0.1:39200 --track-path=$track --client-options="timeout:60" --challenge=$1 --kill-running-processes --on-error=abort --race-id=${RACE_ID}
+    esrally race --pipeline=benchmark-only --target-host=127.0.0.1:39200 --track-path=$track --client-options="timeout:60" --challenge=$1 --kill-running-processes --on-error=abort --race-id=$(uuidgen)
     clear_cache_wait
 }
 
 INSTALLATION_ID=`esrally install --quiet --distribution-version=7.12.0 --node-name="rally-node-0" --network-host="127.0.0.1" --http-port=39200 --master-nodes="rally-node-0" --seed-hosts="127.0.0.1:39300" --car="4gheap,x-pack-monitoring-http" --car-params=car_params.json | jq --raw-output '.["installation-id"]'`
 
-export RACE_ID=$(uuidgen)
 esrally start --installation-id="${INSTALLATION_ID}" --race-id="${RACE_ID}"
 
 race "aggregations_without_cache"
