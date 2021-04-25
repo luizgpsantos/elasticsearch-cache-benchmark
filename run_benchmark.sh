9eb11523c07975a47224b9f77e2f5a5593f25b35
Luiz Guilherme P. Santos <luizgpsantos@gmail.com>
2021-04-25 09:13:36 -0300

    car parameters

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,12 +2,12 @@ set -x
 
 exec > >(tee "$HOME/benchmark_$(date +"%Y_%m_%d_%I_%M_%p").log") 2>&1
 
-while getopts mh:mp:mpw:t: flag
+while getopts h:p:w:t: flag
 do
     case "${flag}" in
-        mh) monitoringhost=${OPTARG};;
-        mp) monitoringport=${OPTARG};;
-        mpw) monitoringpw=${OPTARG};;
+        h) monitoringhost=${OPTARG};;
+        p) monitoringport=${OPTARG};;
+        w) monitoringpw=${OPTARG};;
         t) track=${OPTARG};;
     esac
 done
@@ -29,7 +29,9 @@ export MONITORING_HOST=$monitoringhost
 export MONITORING_PORT=$monitoringport
 export MONITORING_PASSWORD=$monitoringpw
 
-INSTALLATION_ID=`esrally install --quiet --distribution-version=7.12.0 --node-name="rally-node-0" --network-host="127.0.0.1" --http-port=39200 --master-nodes="rally-node-0" --seed-hosts="127.0.0.1:39300" --car="4gheap,x-pack-monitoring-http" --car-params=car_params.json | jq --raw-output '.["installation-id"]'`
+sed -i -e 's/CLUSTER_NAME/${CLUSTER_NAME}/' -e 's/MONITORING_HOST/${MONITORING_HOST}/' -e 's/MONITORING_PORT/${MONITORING_PORT}/' -e 's/MONITORING_PASSWORD/${MONITORING_PASSWORD}/' car_params.json | tee car_params_concrete.json
+
+INSTALLATION_ID=`esrally install --quiet --distribution-version=7.12.0 --node-name="rally-node-0" --network-host="127.0.0.1" --http-port=39200 --master-nodes="rally-node-0" --seed-hosts="127.0.0.1:39300" --car="4gheap,x-pack-monitoring-http" --car-params=car_params_concrete.json | jq --raw-output '.["installation-id"]'`
 
 esrally start --installation-id="${INSTALLATION_ID}" --race-id="${RACE_ID}"
 
